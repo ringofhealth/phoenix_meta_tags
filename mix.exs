46c64e32db56efc497a995eca71b1bd92d3f3596
long <hlongvu@gmail.com>
2021-08-22 16:49:29 +0700

    increase version 0.1.9

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule PhoenixMetaTags.MixProject do
   def project do
     [
       app: :phoenix_meta_tags,
-      version: "0.1.8",
+      version: "0.1.9",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
       description: description(),
